bd8849081baf541c62b6bc4d7321c037af709d4d
Dragoș Efrem <101568589+dragosefrem@users.noreply.github.com>
2022-05-02 20:40:24 +0300

    Initialised project variables and structures

---
+++ src/tree-displayer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define n 31
+#define lch 10
+#define index n
+
+typedef char alfa[lch];
+
+char ch;
+int k1,k2; // identificator sau cheie
+alfa id; /* identificator sau cheie */
+alfa chei[n + 1];
+int i,j,k;
+int a[n + 1];
+int b[index];
+int p[index][index], w[index][index];
+int r[index][index];
+int r[index][index];
+int suma,sumb;
+char litere[27];
+char cifre[11];
+
+int main() {
+
+    return 0;
+}
